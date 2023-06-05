46769c8162fbb3499395c2fe9fc4bc90c2527028
Burcu Bayraö <burju7@yahoo.com>
2023-06-05 12:57:27 +0300

    assingment completed

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMfunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </div>
+  );
 }
+export default ColoradoStateParks;
+console.log(RMfunctions.trees);
